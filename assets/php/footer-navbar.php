9f3de443f59d1aab0e011e76d7a7016992ae053b
pdhaval <dhavalpatel738@gmail.com>
2018-04-08 14:30:02 +0530

    Front page
    - carousel
    - card deck
    - footer navbar
    - php file for hearder nd footer nav bar

---
+++ assets/php/footer-navbar.php
@@ -0,0 +1,8 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+    <li class="nav-item">
+      <p class="text-secondary">Copyright <i class="fa fa-copyright" aria-hidden="true"></i> 2018 <a href="http://www.neatceramic.com" style="text-decoration: none;"><span class="text-secondary font-weight-bold">Neat Ceramic LLP</span></a>. All rights reserved.</p>
+      <p class="text-secondary">Made with <i class="fa fa-heart" aria-hidden="true"></i> & <i class="fa fa-coffee" aria-hidden="true"></i>  in Morvi, Gujarat</p>
+    </li>
+  </ul>
+</nav>
--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/font-awesome/css/font-awesome.min.css">
 
     <!-- Google font -->
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
@@ -17,14 +18,116 @@
             font-family: 'Montserrat', sans-serif;
         }
 
+        .jumbotron {
+            background-image: url('assets/images/jumbotron.jpg');
+            background-size: cover;
+            height: 100vh;
+        }
+
+        #carouselExampleIndicators {
+            margin-top: 0 !important;
+        }
+
+        .carousel-item {
+          height: 500px;
+          object-fit: cover;
+        }
+
+        .card {
+          border: none;
+        }
+
+        .card img {
+          border-radius: 50%;
+        }
+
+
     </style>
 
     <title>Neat Ceramic LLP | Sanitary manufacturer</title>
   </head>
   <body>
+    <!-- header navbar-->
       <?php
           echo file_get_contents('assets/php/header-navbar.php');
        ?>
+
+    <!-- Jumbotron -->
+    <div class="jumbotron text-white mb-0">
+      <h1 class="display-4">Hello, world!</h1>
+      <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+      <hr class="my-4">
+      <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+      <p class="lead">
+        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+      </p>
+    </div>
+
+    <!-- Slider with navigation -->
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="d-block w-100" src="assets/images/slider-1.jpg" alt="First slide">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src="assets/images/slider-2.jpg" alt="Second slide">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src="assets/images/slider-3.jpg" alt="Third slide">
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+
+    <!--  card-deck-->
+    <div class="container">
+
+
+    <div class="card-deck mt-4">
+    <div class="card">
+      <img class="card-img-top rounded-circle" src="assets/images/card.jpg" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+    <div class="card">
+      <img class="card-img-top rounded-circle" src="assets/images/card.jpg" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+    <div class="card">
+      <img class="card-img-top rounded-circle" src="assets/images/card.jpg" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+    </div>
+    </div>
+
+    <?php
+      echo file_get_contents('assets/php/footer-navbar.php');
+    ?>
+
+
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
